621e9f0b62c304fc70af6f372f27a3f56905ffb0
Liyichun <378820235@qq.com>
2017-02-16 14:25:52 +0800

    init

---
+++ src/js/page/list.js
@@ -0,0 +1,14 @@
+//引入css
+require("../../css/lib/reset.css");
+require("../../css/common/global.css");
+require("../../css/common/grid.css");
+require("../../css/page/list.less");
+//require("../../js/lib/bootstrap/bootstrap.js");
+
+
+var html = '';
+for(var i=0;i<5;i++){
+	html += '<li>列表'+(i+1)+'</li>';
+}
+
+$('#list').html(html);
